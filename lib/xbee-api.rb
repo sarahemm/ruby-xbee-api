ae1709f3464b71ff3363556b2257a4fc828ffd22
Hacklab <hacklab@hacklab-MP061.(none)>
2013-11-02 22:41:12 -0400

    Not working yet, but closer. Sending what looks like the right data, but still no response from the XBee. Sending identical data from a terminal works though :|

--- lib/xbee-api.rb
+++ lib/xbee-api.rb
@@ -13,23 +13,15 @@ module XBee
       checksum = 0
       data.map {|byte| checksum += byte.ord}
       checksum = 0xFF - (checksum & 0xFF)
-      length_h = data.length & 0xFF
-      length_l = (data.length >> 8) & 0xFF
-      print "Writing command: 0x7e 0x#{length_l.to_s(16)} 0x#{length_h.to_s(16)} [ "
-      data.map {|byte| print "0x#{byte.ord.to_s(16)} "}
-      puts "] 0x#{checksum.to_s(16)}"
-      @port.write 0x7E
-      @port.write length_l
-      @port.write length_h
-      data.map {|byte| @port.write byte}
-      @port.write checksum
+      @port.write [ 0x7E, data.count, data.map {|b| b.kind_of?(String) ? b.ord : b}, checksum ].flatten.pack("CnC#{data.count}C")
+      [ 0x7E, data.count, data.map {|b| b.kind_of?(String) ? b.ord : b}, checksum ].flatten.pack("CnC#{data.count}C").each_char {|b| print "0x#{b.ord.to_s(16)} "}
     end
     
     def at_command(at_cmd)
       cmd_pkt = [0x08, 0x01]
       at_cmd.split(//).map {|byte| cmd_pkt.push byte}
       send_packet cmd_pkt
-      return @port.read
+      return @port.read 1
     end
     
     def node_identifier
